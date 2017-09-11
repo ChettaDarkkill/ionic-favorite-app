c5585663c8b40796f59e464268b6001610ce3370
Chetta <darkkill.bymooy@gmail.com>
2017-09-12 00:35:51 +0700

    add hamburger

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,26 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, MenuController } from 'ionic-angular';
+import { NavController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { TabsPage } from '../pages/tabs/tabs';
+import { SettingsPage } from '../pages/settings/settings';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = TabsPage;
+  // rootPage:any = TabsPage;
+  tabsPage = TabsPage;
+  settingsPage = SettingsPage;
+  @ViewChild('nav') nav: NavController;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(
+    private menuCtrl: MenuController,
+    platform: Platform, 
+    statusBar: StatusBar, 
+    splashScreen: SplashScreen) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -19,5 +28,11 @@ export class MyApp {
       splashScreen.hide();
     });
   }
+
+  onload(page: any){
+    this.nav.setRoot(page);
+    this.menuCtrl.close();
+  }
 }
+
 
